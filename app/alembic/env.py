afae3992702696790a5f59d6aa8858b412c778a9
Juan David <david.rodriguez@endava.com>
2020-05-09 13:01:41 -0500

    fixing alembic import

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -1,9 +1,8 @@
 from logging.config import fileConfig
 
+from alembic import context
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
-from alembic import context
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -18,7 +17,7 @@ sys.path = ['', '..'] + sys.path[1:]
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
-from main import models
+from models import models
 
 # target_metadata = mymodel.Base.metadata
 target_metadata = models.Base.metadata
