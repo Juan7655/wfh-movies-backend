08dc25e533578de98155225a84035458fb561d6a
Juan David Rodríguez Cuervo <juan.7655@hotmail.com>
2021-12-09 12:53:01 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
 from pydantic import BaseSettings
+import os
 import logging as log
 
 log.basicConfig(level=log.DEBUG)
 
 
 class Settings(BaseSettings):
-    database_url: str
-    SQLALCHEMY_DATABASE_URI: str = 'postgresql://postres:admin123@localhost:5432/wfh-movies'
+    database_url: str = os.environ["HEROKU_POSTGRESQL_IVORY_URL"]
+    SQLALCHEMY_DATABASE_URI: str = os.environ["HEROKU_POSTGRESQL_IVORY_URL"]
     audit: str = ''
     api_key: str = ''
 
